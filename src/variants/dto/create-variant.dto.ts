921d6052194a844f8e0b5e1332536769fbbc95bf
Ilya Tovpeko <tovpekoilya@gmail.com>
2025-06-26 10:46:09 +0300

    add casting to number type

--- src/variants/dto/create-variant.dto.ts
+++ src/variants/dto/create-variant.dto.ts
@@ -1,9 +1,11 @@
 import { IsString, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateVariantDto {
   @IsString()
   readonly option: string;
 
+  @Type(() => Number)
   @IsNumber()
   readonly distribution: number;
 }
